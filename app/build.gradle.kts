a35f394f43c66c4784bb0227098e5f06934cbfa0
WRyouAldi <62519489+WRyouAldi@users.noreply.github.com>
2026-09-16 14:08:51 +0700

    Add app Gradle configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.naufalfarma.apotek"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.naufalfarma.apotek"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 14
+        versionName = "1.4"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+    implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
+}
